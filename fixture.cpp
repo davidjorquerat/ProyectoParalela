aa51ad7384efec4e3c3535ab8f8dc5d19afbc98e
david jorquera <david_jorquera@hotmail.com>
2017-11-20 00:33:07 -0300

    adicion de la funcion para calcular la distancia con long y lat

--- fixture.cpp
+++ fixture.cpp
@@ -5,7 +5,14 @@
 
 using namespace std;
 
+double distancia_long_lat(float lat1, float long1, float lat2, float long2)//los calculos son en km
+{
+	float radio_tierra = 6378.7;
+
+	return radio_tierra*acos(((sin(lat1))*(sin(lat2)))+(((cos(lat1))*(cos(lat2))*(cos(long2 - long1)))));
+}
+
 int main(int argc, char *argv[])
 {
-	
+
 }
